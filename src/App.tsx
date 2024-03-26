0dd2ec82f5e4aa9ca6d514a39d4be24f1d603946
Saeid Mohadjer <saeid@fastmail.fm>
2024-03-26 11:54:10 +0100

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ export default function App() {
   return (
     <>
         <h1>Signup Form</h1>
-        <Form method="POST" action="/api/signup" fields={signupFields} buttonLabel="Sign Up" />
+        <Form method="POST" action="/api/signup" fields={signupFields} label="Sign Up" />
         <hr />
         <h1>Login Form</h1>
-        <Form method="POST" action="/api/login" fields={loginFields} buttonLabel="Login" />
+        <Form method="POST" action="/api/login" fields={loginFields} label="Login" />
     </>
   )
 }
